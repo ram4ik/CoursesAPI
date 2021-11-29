8ad272b64d08daa42d070ade1dc892317891bc6a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-29 12:55:16 +0200

    Initial Commit

---
+++ CoursesAPI/CoursesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesAPIApp.swift
+//  CoursesAPI
+//
+//  Created by ramil on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoursesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
